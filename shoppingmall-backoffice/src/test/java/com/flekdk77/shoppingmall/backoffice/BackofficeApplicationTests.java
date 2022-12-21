df994c3055f43a6ffb64575eb9285c85bd9dd19b
younghun0 <flekdk77@gmail.com>
2022-12-21 16:23:55 +0900

    Add shoppingmall-backoffice project.

---
+++ shoppingmall-backoffice/src/test/java/com/flekdk77/shoppingmall/backoffice/BackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flekdk77.shoppingmall.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
