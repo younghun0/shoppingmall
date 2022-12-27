60335369998758ab8407ed024e14aca35faee044
younghun0 <flekdk77@gmail.com>
2022-12-27 18:57:45 +0900

    dto 수정

--- shoppingmall-backoffice/src/main/java/com/flekdk77/shoppingmall/backoffice/dto/Event.java
+++ shoppingmall-backoffice/src/main/java/com/flekdk77/shoppingmall/backoffice/dto/Event.java
@@ -1,19 +1,42 @@
 package com.flekdk77.shoppingmall.backoffice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Event {
-    private int id;
-    private String title;
-    private String content;
-    private String created_at;
-    private int created_by;
+    int id;
+    String title;
+    String content;
+    String created_at;
+    int created_by;
 
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
+    public String getCreated_at(){
+        return created_at;
+    }
+    public void setCreated_at(String created_at){
+        this.created_at=created_at;
+    }
+    public int getCreated_by(){
+        return created_by;
+    }
+    public void setCreated_by(int created_by){
+        this.created_by=created_by;
+    }
 }
